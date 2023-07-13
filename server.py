247226c3b9efe029abbec4eaeac131e6f751d5b0
David Haas <davidhaas6@gmail.com>
2023-07-13 17:22:43 -0500

    created fastapi server

--- server.py
+++ server.py
@@ -1,14 +1,39 @@
-
-word1 = "Hello"
-word2 = "World"
-openscad_path = "C:/Program%20Files/OpenSCAD/openscad.com"
-program_path = "C:/Users/David/Documents/Projects/cadwebsite/names.scad"
-var1 = f'-D name1=\"{word1}\"'
-var2 = f'-D name2=\"{word2}\"'
-build = f" -o './test.stl' '{program_path}'"
-command = f"{openscad_path} {var1} {var2} {build}"
-print(command)
-
-# execute command
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import os
-os.system(command)
+
+app = FastAPI()
+
+class DuoViewModel(BaseModel):
+    word1: str
+    word2: str
+    output_path: str
+
+def create_duoview_model(word1: str, word2: str, output_path: str) -> str:
+    def add_variable(name, value):
+        return f"-D {name}='\"{value}\"'"
+
+    openscad_path = "/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
+    program_path = "/Users/davidhaas/projects/cad-generation/names.scad"
+    command = (
+        f"{openscad_path} -o '{output_path}' '{program_path}' "
+        + f'{add_variable("name1",word1)} {add_variable("name2",word2)}'
+    )
+    print(command)
+    os.system(command)
+    return output_path
+
+
+@app.post("/create_model/")
+async def create_model(data: DuoViewModel):
+    try:
+        stl_path = create_duoview_model(data.word1, data.word2, data.output_path)
+        return {"message": "Successfully created model", "stl_path": stl_path}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    output_path = os.path.join(os.getcwd(), "test.stl")
+    stl_path = create_duoview_model("DAVE", "HAAS", output_path)
+    print(stl_path)
